84953c400b66ba9993094d6799b14f08bd5c3b7c
Adam Rackis <adamrackis@hotmail.com>
2022-09-20 12:02:12 -0500

    set cache header for shoelace bundle

--- next.config.js
+++ next.config.js
@@ -2,6 +2,20 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
 
-module.exports = nextConfig
+  async headers() {
+    return [
+      {
+        source: "/shoelace/shoelace-bundle-:hash.js",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public,max-age=31536000,immutable",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
