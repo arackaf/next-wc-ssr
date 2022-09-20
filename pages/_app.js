d6531a3917fcfe9d688985f4213300a1b9c9ea0b
Adam Rackis <adamrackis@hotmail.com>
2022-09-20 11:51:50 -0500

    Turn off service worker

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,6 @@
 import "@shoelace-style/shoelace/dist/themes/light.css";
 import "../styles/globals.css";
 
-if (typeof window === "object" && !/localhost/.test(location.href) && "serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("/sw.js")
-    .then((registration) => {
-      console.log("Service worker registered", registration);
-    })
-    .catch((err) => {
-      console.log("Error registering service worker", err);
-    });
-}
-
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
